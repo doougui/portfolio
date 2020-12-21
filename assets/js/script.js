848704028113ecd9af6e4169fd34a0e77dd56ca1
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2020-12-21 19:43:29 -0300

    Adding selection to projects filters

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,33 @@ function setMenuPosition() {
   });
 }
 
+const projectFilters = $l('button[data-filter]');
+
+function addOutlineClass() {
+  projectFilters.forEach(item => {
+    const child = item.querySelector('.button__aesthetic');
+
+    if (!child.classList.contains('button__aesthetic--outline')) {
+      child.classList.add('button__aesthetic--outline');
+    }
+  });
+}
+
+function removeOutlineClass(el) {
+  if (el.classList.contains('button__aesthetic--outline')) {
+    el.classList.remove('button__aesthetic--outline');
+  }
+}
+
+function selectProjectFilter(e) {
+  e.preventDefault();
+
+  addOutlineClass();
+  removeOutlineClass(e.target);
+}
+
 setMenuPosition();
+
+projectFilters.forEach(item => {
+  item.addEventListener('click', selectProjectFilter);
+});
