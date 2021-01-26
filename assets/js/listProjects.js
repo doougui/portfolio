5165d067905c7459874c6ca236be77c723840844
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-01-26 15:47:09 -0300

    Listing projects based on json file

--- assets/js/listProjects.js
+++ assets/js/listProjects.js
@@ -1,3 +1,10 @@
 projectsJson.map((item, index) => {
-  
+  const projectItem = $('.models .project').cloneNode(true);
+
+  projectItem.setAttribute('data-category', item.categoryKey);
+  projectItem.style.backgroundImage = `url(${item.image})`;
+
+  projectItem.addEventListener('click', openModal);
+
+  $('.projects__container').append(projectItem);
 });
