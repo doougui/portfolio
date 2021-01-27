d5cf16c1dd3d365abc82378b97644ee7b4ba0927
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-01-27 14:02:35 -0300

    Animating on scroll

---
+++ assets/js/scroll.js
@@ -0,0 +1,19 @@
+const sections = $l('.js-scroll');
+
+if (sections.length) {
+  const halfWindow = window.innerHeight * 0.6;
+  
+  function animateScroll() {
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - halfWindow) < 0;
+  
+      if (isSectionVisible) {
+        return section.classList.add('active');
+      }
+    });
+  }
+  
+  animateScroll();
+  window.addEventListener('scroll', animateScroll);
+}
