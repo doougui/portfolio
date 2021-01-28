a3a7a7e173c3e28b68b843fe740cf6231ee8e5ab
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-01-28 14:55:28 -0300

    Closing mobile menu on outside click

--- assets/js/mobileNav.js
+++ assets/js/mobileNav.js
@@ -7,3 +7,24 @@ function openMenu(e) {
 }
 
 menuButton.addEventListener('click', openMenu);
+
+outsideClick(menuList, _ => {
+  menuButton.classList.remove('burger__wrapper--active');
+  menuList.classList.remove('navigation__items--active');
+});
+
+function outsideClick(element, callback) {
+  const html = document.documentElement;
+  html.addEventListener('click', handleOutsideClick);
+
+  function handleOutsideClick(e) {
+    if (
+      (
+        !element.contains(e.target)  // Se o elemento clicado não for filho do nav items
+        && !menuButton.contains(e.target) // Se o elemento clicado não for filho do hamburger
+      ) 
+    ) {
+      callback();
+    }
+  }
+}
