2fefa34f2f1a7d3207ee6733122c5dbc3c98fd66
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-02-03 10:09:52 -0300

    Animation fun fact on open and close accordion

--- assets/js/modules/accordion.js
+++ assets/js/modules/accordion.js
@@ -1,15 +1,34 @@
-import { $l } from '../utils/selectors.js';
+import { $, $l } from '../utils/selectors.js';
 
 export default function initAccordion() {
   const accordionOpeners = $l('.accordion__opener');
 
   if (accordionOpeners.length) {
-    function activeAccordion() {
-      this.nextElementSibling.classList.toggle('accordion__content--visible');
+    function toggleAccordion() {
+      const visibleClass = 'accordion__content--visible';
+      const hidingClass = 'accordion__content--hiding';
+
+      if (this.nextElementSibling.classList.contains(visibleClass)) {
+        this.nextElementSibling.classList.remove(visibleClass);
+        this.nextElementSibling.classList.add(hidingClass);
+
+        const animationDuration = parseFloat(
+          getComputedStyle($(`.${hidingClass}`))['animationDuration']
+        ) * 1000;
+
+        setTimeout(() => {
+          this.nextElementSibling.classList.remove(hidingClass);
+        }, animationDuration);
+
+        return;
+      }
+
+      this.nextElementSibling.classList.add(visibleClass);
+      return;
     }
 
     accordionOpeners.forEach(opener => {
-      opener.addEventListener('click', activeAccordion);
+      opener.addEventListener('click', toggleAccordion);
     });
   }
 }
