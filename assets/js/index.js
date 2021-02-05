66662f52498c67189f4b0f2e019a5110d3a82241
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-02-04 21:53:26 -0300

    Preloading modules

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 
 import initNav from './modules/nav.js';
 import initMobileNav from './modules/mobileNav.js';
+import initScroll from './modules/scroll.js';
 import initProjectList from './modules/listProjects.js';
 import initModal from './modules/modal.js';
-import initProjectFilters from './modules/projectFilters.js';
-import initScroll from './modules/scroll.js';
-import initFooterYear from './modules/footerYear.js';
 import initAccordion from './modules/accordion.js';
+import initProjectFilters from './modules/projectFilters.js';
+import initFooterYear from './modules/footerYear.js';
 
 initNav();
 initMobileNav();
